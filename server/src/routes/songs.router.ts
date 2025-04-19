d56d30a7fa75690aac1a8d490be3324d7afffcce
Nati Gurevich <nati_g4@hotmail.com>
2025-04-20 01:42:22 +0300

    Fix search

--- server/src/routes/songs.router.ts
+++ server/src/routes/songs.router.ts
@@ -9,12 +9,12 @@ import { isAdmin } from "../utils/auth";
 
 export const songsRouter = express.Router();
 
+songsRouter.get("/search", isAdmin, searchSongs);
+
 songsRouter.get("/:id", getSongById);
 
 songsRouter.use(isAdmin);
 
 songsRouter.get("/", getAllSongs);
-
-songsRouter.get("/search", searchSongs);
 
 songsRouter.post("/", addSong);
